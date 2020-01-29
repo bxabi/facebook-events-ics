9d09b0b63735dec30cb02356635dbb5f6b76c60f
bxabi <bxabi@users.noreply.github.com>
2020-01-29 11:52:37 +0100

    Update update-facebook-events.sh

--- update-facebook-events.sh
+++ update-facebook-events.sh
@@ -1,4 +1,4 @@
 curl --silent "<your facebook events url>" -o ~/.config/facebook-events/facebook-events.ics >/dev/null
 
-# if divide-events.py is not in the current folder, then add your path to it here
+# if divide-events.py is not in the current folder, then add your path to it here instead of the './'
 python ./divide-events.py >/dev/null

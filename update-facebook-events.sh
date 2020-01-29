f0628fefc6479f4c89f883eca9d969f7dd34bfde
bxabi <bxabi@users.noreply.github.com>
2020-01-29 11:49:40 +0100

    more understandable path definition
    It runs without changes if the divide script is in the current folder

--- update-facebook-events.sh
+++ update-facebook-events.sh
@@ -1,2 +1,4 @@
 curl --silent "<your facebook events url>" -o ~/.config/facebook-events/facebook-events.ics >/dev/null
-python <path/>divide-events.py >/dev/null
+
+# if divide-events.py is not in the current folder, then add your path to it here
+python ./divide-events.py >/dev/null

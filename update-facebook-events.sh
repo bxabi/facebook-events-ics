e77d4b9c1c405e604941a338721f93b26e60e87e
bxabi <csabi@bxabi.com>
2020-01-30 11:35:17 +0100

    create config folder if doesn't exist yet

--- update-facebook-events.sh
+++ update-facebook-events.sh
@@ -1,3 +1,4 @@
+mkdir -p ~/.config/facebook-events/
 curl --silent "<your facebook events url>" -o ~/.config/facebook-events/facebook-events.ics >/dev/null
 
 # if divide-events.py is not in the current folder, then add your path to it here instead of the './'

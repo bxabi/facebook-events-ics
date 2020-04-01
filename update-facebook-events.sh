d5cff4c2639590a4d3c6464190dfe56af921dfa3
bxabi <csabi@bxabi.com>
2020-04-01 16:04:08 +0200

    explicitly calling python3 instead of python in the script.

--- update-facebook-events.sh
+++ update-facebook-events.sh
@@ -2,4 +2,4 @@ mkdir -p ~/.config/facebook-events/
 curl --silent "<your facebook events url>" -o ~/.config/facebook-events/facebook-events.ics >/dev/null
 
 # if divide-events.py is not in the current folder, then add your path to it here instead of the './'
-python ./divide-events.py >/dev/null
+python3 ./divide-events.py >/dev/null
